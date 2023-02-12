10bac7506f1729bb937df53073f93c7bb33ce514
Layla Barbosa <122912928+laylabarbosa@users.noreply.github.com>
2023-02-11 23:42:19 -0500

    add main class

---
+++ PinkPongGame.java
@@ -0,0 +1,15 @@
+/**	The PinkPongGame program implements an application that
+* create an object for inal project in Programming fundamentals course at Georgian at Ilac course. 
+*	Author: Layla de Souza Barbosa & Lizandra Esterque
+*	Date: 11/25/2022
+*	Time: 00:43 AM
+*/
+
+public class PinkPongGame {
+
+    public static void main(String[] args) {
+        
+        //windown where we play
+        GameFrame frame = new GameFrame();
+    }
+}
